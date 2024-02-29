9fa09f28bd7a47abfd77ebab51d7eb1857aa9f1a
julioaraujo96 <julioaraujo.bs@gmail.com>
2024-02-29 09:51:16 +0000

    chore: socket init

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,13 @@
 import { createServer } from 'http';
+import { Server, Socket } from 'socket.io';
 
 const server = createServer();
+const io = new Server(server);
+
+io.on('connection', (socket : Socket) => {
+    console.log('Client connected', socket.id);
+})
+
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
