6b21025a202141ac296c7546ea1eaa9d768d18e7
julioaraujo96 <julioaraujo.bs@gmail.com>
2024-06-18 10:20:38 +0100

    fix:: remove xhr poll error

--- client/src/lib/webSocketConnection.ts
+++ client/src/lib/webSocketConnection.ts
@@ -2,6 +2,9 @@ import ioClient from 'socket.io-client';
 
 const ENDPOINT = import.meta.env.VITE_SERVER_URL;
 
-const socket = ioClient(ENDPOINT, { path: '/api/' });
+const socket = ioClient(ENDPOINT, {
+  path: '/api/',
+  transports: ['websocket'],
+});
 
 export const io = socket;
